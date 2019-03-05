09fb7bc0d28cb7752d30a4a727527514758a9718
Kentros Alexandros <crAlexcomp@gmail.com>
2019-03-05 17:39:25 +0200

    Fixes!

--- src/app.js
+++ src/app.js
@@ -52,7 +52,7 @@ const init = () => {
   //Players
   document.querySelector(".player-"+0+"-panel").classList.remove("winner")
   document.querySelector(".player-"+1+"-panel").classList.remove("winner")
-  document.querySelector(".player-"+0+"-panel").classList.remove("active")
+  document.querySelector(".player-"+0+"-panel").classList.add("active")
   document.querySelector(".player-"+1+"-panel").classList.remove("active")
 };
 
